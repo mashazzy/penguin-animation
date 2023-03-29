94de716a4ad727219e40b7295c73a5044cafe87b
mashazzy <massa.zupcic@gmail.com>
2023-03-29 15:14:11 +0200

    added js
    added js

---
+++ index.js
@@ -0,0 +1,10 @@
+var button = document.getElementsByClassName('backgroundtrigger');
+document.addEventListener('click', function (button) {
+    
+    var backgrounds = document.getElementsByClassName('background');
+backgrounds[0].classList.add('blackbackground')
+backgrounds[1].classList.add('blackbackground')
+backgrounds[2].classList.add('blackbackground')
+
+
+}, false);
